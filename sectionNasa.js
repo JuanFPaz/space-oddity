abe06dde4a7ade951ae9be7eb673c84af509ea3a
Juan Paz <jpaz.dev94@gmail.com>
2023-12-19 16:41:52 -0300

    Subiendo nuevos avances

--- sectionNasa.js
+++ sectionNasa.js
@@ -1,3 +1,5 @@
+import cat from './cat.jpg'
+
 const queryString = window.location.search
 // Crear un objeto URLSearchParams para manejar los parámetros
 const urlParams = new URLSearchParams(queryString)
@@ -5,24 +7,31 @@ const DATE = urlParams.get('date')
 const APIKEY = '9d3ii44W8GRaQvkghYy3Om9cRI7Chb3ZQM8jx1d0'
 
 export default function sectionNasa () {
-  fetch(`https://api.nasa.gov/planetary/apod?date=${DATE}&api_key=${APIKEY}`)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.code === 400) {
-        throw Error(data.msg)
-      }
-      const { title, date, hdurl, explanation } = data
-      document.querySelector('div.main section').innerHTML = `
+  if (DATE) {
+    fetch(`https://api.nasa.gov/planetary/apod?date=${DATE}&api_key=${APIKEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.code === 400) {
+          throw Error(data.msg)
+        }
+        const { title, date, hdurl, explanation } = data
+        document.querySelector('div.main section').innerHTML = `
         <h1>${title}</h1>
         <h2>${date}</h2>
         <img src=${hdurl}>
         <p>${explanation}</p>
     `
-    })
-    .catch((err) => {
-      console.log(err)
-      document.querySelector('div.main section').innerHTML = `
+      })
+      .catch((err) => {
+        console.log(err)
+        document.querySelector('div.main section').innerHTML = `
         <h1>Error: ${err.message}</h1>
     `
-    })
+      })
+  } else {
+    document.querySelector('div.main section').innerHTML = `
+        <h1>Busca una Fecha en la barrita de busqueda, y mira la foto q sale :D</h1>
+        <img id=cat src=${cat}>
+        `
+  }
 }
