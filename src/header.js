f7d9deba75898f6d535d5c5d2a2dc660b45eaacc
Juan Paz <jpaz.dev94@gmail.com>
2024-07-04 11:57:35 -0300

    Estableciendo el header para que NO se traduzca

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 async function createHeader () {
   document.querySelector('header').innerHTML = `
     <h1>
-      <a href='/'>
+      <a href='/' translate=no>
         spaceoddity.jpaz.ar
       </a>
      </h1>
